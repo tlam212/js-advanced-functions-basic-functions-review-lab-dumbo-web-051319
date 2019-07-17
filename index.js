82487bb94930db804bb98ebba2c1d76ae05e7f51
Toni Lamont <tlam212@gmail.com>
2019-07-17 19:44:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 
 function saturdayFun(activity="roller-skate"){
-  return (`This Saturday, I want to ${activity}!`)
+  return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office"){
+  return ""
 }
